1cc5b7f1f151fab8af60d07a5c0b7ec9bc0dfbd1
xgamehades <ninhducthien2k@gmail.com>
2022-06-26 15:52:59 +0700

    Initial commit

---
+++ src/main/java/datn_quanlynhahang/DatnQuanLyNhaHangApplication.java
@@ -0,0 +1,13 @@
+package datn_quanlynhahang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnQuanLyNhaHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnQuanLyNhaHangApplication.class, args);
+    }
+
+}
